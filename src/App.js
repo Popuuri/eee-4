1afe613957e722c920ff0c8f394d2cc1200853c5
popuuri <popurimourya@gmail.com>
2023-10-14 18:58:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
+import './cp.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const ColorPicker = () => {
+ const [color, setColor] = useState('#000000');
+
+ const handleChange = (newColor) => {
+    setColor(newColor.hex);
+ };
+
+ return (
+    <div className="color-picker">
+      <SketchPicker color={color} onChange={handleChange} />
+      <p>Selected Color: {color}</p>
     </div>
-  );
-}
+ );
+};
 
-export default App;
+export default ColorPicker;
+
